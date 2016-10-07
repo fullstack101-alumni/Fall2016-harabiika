282e27471ca134c25100c523d602538cc63f9476
Nikolay Babev <nikolay.babev@st.ovgu.de>
2016-10-07 13:02:01 +0300

    changed a popup div

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 
-
 $( "td" ).click(function() {
   $("#overlay").slideDown();
   $("#overlay").css('display', 'inline');
-  
-
+  $('#main').css('filter' , 'blur(4px)');
+  //$("td").addClass("disableHover");
+  $( "td" ).css({'pointer-events': 'none'});
 });
 
 $('#overlay').click(function(){
   $( this ).slideUp();
+   $('#main').css('filter' , 'blur(0px)');
+   $( "td ").css({'pointer-events': 'auto'});
 });
 
