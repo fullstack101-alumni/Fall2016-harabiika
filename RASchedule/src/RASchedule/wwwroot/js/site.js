cd83b2a2cffdf7c39428ea30e14a3b27bf7f5df9
Katalina Dimitrova <katalina.dimitrova@st.ovgu.de>
2016-10-14 13:34:16 +0300

    started adding days as ids, added cancel button, added TODO

--- RASchedule/src/RASchedule/wwwroot/js/site.js
+++ RASchedule/src/RASchedule/wwwroot/js/site.js
@@ -1,10 +1,13 @@
 ﻿// Write your Javascript code.
 
+var $clicked = "";
 
 $("td").click(function () {
     $("#overlay").slideDown();
     $("#overlay").css('display', 'inline');
     $('#main').css('filter', 'blur(4px)');
+    $clicked = this;
+   
     //$("td").addClass("disableHover");
     $("td").css({ 'pointer-events': 'none' });
 });
@@ -22,3 +25,17 @@ $(document).keydown(function (e) {
         $("td ").css({ 'pointer-events': 'auto' });
     }
 });
+
+$("#takehour").click(function (e) {
+    // takes current count of hours from DB, 
+    // adds 1
+    // puts them again in the DB
+
+    // take the id and put in the DB
+    console.log($clicked.id);
+});
+
+// profile button send weekly report 
+// profile button send night duty report
+// a bot mail sends email to ilko
+// when weekly report is sent, current hours = 0, current duty days = 0
